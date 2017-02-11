baa808e1ae28f5def4ed3b419ba1ee30a8c570e0
priyesh1224 <pvasani187@gmail.com>
2017-02-12 01:21:05 +0530

    Add files via upload

---
+++ home.php
@@ -0,0 +1,26 @@
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html>
+<body>
+<h1>HOME</h1>
+<h3>
+<?php if(@$_SESSION["authenticated"]) { ?>
+ you are logged in !!!!! 
+<br>
+<a href="logout.php">logout</a>
+<?php } else { ?>
+you are not logged in 
+<?php } ?>
+</h3>
+<br>
+<b>login demos </b>
+<ul>
+<li><a href="login1.php">login1</a></li>
+<li><a href="login2.php">login2</a></li>
+<li><a href="login3.php">login3</a></li>
+<li><a href="login4.php">login4</a></li>
+</ul>
+</body>
+</html>
